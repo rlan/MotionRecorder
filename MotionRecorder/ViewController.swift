13e5a697d8544a258804402be90fe0db117d7ef2
Rick Lan <rlan@users.noreply.github.com>
2018-06-26 08:47:45 +0900

    Initial Commit

---
+++ MotionRecorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotionRecorder
+//
+//  Created by Lan, Rick on 2018/06/26.
+//  Copyright © 2018 Lan, Rick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
